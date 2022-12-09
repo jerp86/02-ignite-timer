725cee6cba6d1e3891f8107faacc9e9a05f4ee65
José Eduardo Rodrigues Pinto Jerp86 <jerp.dev@gmail.com>
2022-12-09 20:37:49 -0300

    add layout

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,10 +1,13 @@
 import { Route, Routes } from 'react-router-dom'
+import { DefaultLayout } from './layouts/DefaultLayout'
 
 import { History, Home } from './pages'
 
 export const Router = () => (
   <Routes>
-    <Route path="/" element={<Home />} />
-    <Route path="/history" element={<History />} />
+    <Route path="/" element={<DefaultLayout />}>
+      <Route path="/" element={<Home />} />
+      <Route path="/history" element={<History />} />
+    </Route>
   </Routes>
 )
