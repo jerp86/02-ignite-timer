819437c7872a651c41e09bd841808dfd067d95c7
José Eduardo Rodrigues Pinto Jerp86 <jerp.dev@gmail.com>
2022-12-09 23:23:52 -0300

    add structure

--- src/pages/History/index.tsx
+++ src/pages/History/index.tsx
@@ -1,1 +1,64 @@
-export const History = () => <h1>History</h1>
+import { HistoryContainer, HistoryList } from './styles'
+
+export const History = () => (
+  <HistoryContainer>
+    <h1>Meu histórico</h1>
+
+    <HistoryList>
+      <table>
+        <thead>
+          <tr>
+            <th>Tarefa</th>
+            <th>Duração</th>
+            <th>Início</th>
+            <th>Status</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Aqui é a tarafa</td>
+            <td>20 minutos</td>
+            <td>Há 2 meses</td>
+            <td>Concluído</td>
+          </tr>
+          <tr>
+            <td>Aqui é a tarafa</td>
+            <td>20 minutos</td>
+            <td>Há 2 meses</td>
+            <td>Concluído</td>
+          </tr>
+          <tr>
+            <td>Aqui é a tarafa</td>
+            <td>20 minutos</td>
+            <td>Há 2 meses</td>
+            <td>Concluído</td>
+          </tr>
+          <tr>
+            <td>Aqui é a tarafa</td>
+            <td>20 minutos</td>
+            <td>Há 2 meses</td>
+            <td>Concluído</td>
+          </tr>
+          <tr>
+            <td>Aqui é a tarafa</td>
+            <td>20 minutos</td>
+            <td>Há 2 meses</td>
+            <td>Concluído</td>
+          </tr>
+          <tr>
+            <td>Aqui é a tarafa</td>
+            <td>20 minutos</td>
+            <td>Há 2 meses</td>
+            <td>Concluído</td>
+          </tr>
+          <tr>
+            <td>Aqui é a tarafa</td>
+            <td>20 minutos</td>
+            <td>Há 2 meses</td>
+            <td>Concluído</td>
+          </tr>
+        </tbody>
+      </table>
+    </HistoryList>
+  </HistoryContainer>
+)
