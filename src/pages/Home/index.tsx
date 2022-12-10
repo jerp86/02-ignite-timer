f79ff226c11c51a4b0e13918215625ab00ed3eea
José Eduardo Rodrigues Pinto Jerp86 <jerp.dev@gmail.com>
2022-12-09 22:27:27 -0300

    add structure

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,1 +1,30 @@
-export const Home = () => <h1>Home</h1>
+import { Play } from 'phosphor-react'
+
+export const Home = () => (
+  <div>
+    <form action="">
+      <div>
+        <label htmlFor="task">Vou trabalhar em</label>
+        <input type="text" id="task" />
+
+        <label htmlFor="minutesAmount">durante</label>
+        <input type="number" id="minutesAmount" />
+
+        <span>minutos.</span>
+      </div>
+
+      <div>
+        <span>0</span>
+        <span>0</span>
+        <span>:</span>
+        <span>0</span>
+        <span>0</span>
+      </div>
+
+      <button type="submit">
+        <Play />
+        Começar
+      </button>
+    </form>
+  </div>
+)
