efc1abe7ae0257a343318ddd63d5727c899fbecc
José Eduardo Rodrigues Pinto Jerp86 <jerp.dev@gmail.com>
2022-12-09 22:33:18 -0300

    add HomeContainer style

--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -1,6 +1,20 @@
 import styled from 'styled-components'
 
-export const HomeContainer = styled.main``
+export const HomeContainer = styled.main`
+  flex: 1;
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+
+  form {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 3.5rem;
+  }
+`
 
 export const FormContainer = styled.div``
 
