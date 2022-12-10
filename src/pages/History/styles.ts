43441b7381f69a9ffc43f341e065ca035ce119c9
José Eduardo Rodrigues Pinto Jerp86 <jerp.dev@gmail.com>
2022-12-09 23:35:08 -0300

    add HistoryList style

--- src/pages/History/styles.ts
+++ src/pages/History/styles.ts
@@ -13,4 +13,50 @@ export const HistoryContainer = styled.main`
   }
 `
 
-export const HistoryList = styled.div``
+export const HistoryList = styled.div`
+  flex: 1;
+  overflow: auto;
+  margin-top: 2rem;
+
+  table {
+    width: 100%;
+    border-collapse: collapse;
+    min-width: 600px;
+
+    th {
+      background-color: ${({ theme }) => theme['gray-600']};
+      color: ${({ theme }) => theme['gray-100']};
+      padding: 1rem;
+      text-align: left;
+      font-size: 0.875rem;
+      line-height: 1.6;
+
+      &:first-child {
+        border-top-left-radius: 8px;
+        padding-left: 1.5rem;
+      }
+
+      &:last-child {
+        border-top-right-radius: 8px;
+        padding-right: 1.5rem;
+      }
+    }
+
+    td {
+      background-color: ${({ theme }) => theme['gray-700']};
+      border-top: 4px solid ${({ theme }) => theme['gray-800']};
+      padding: 1rem;
+      font-size: 0.875rem;
+      line-height: 1.6;
+
+      &:first-child {
+        width: 50%;
+        padding-left: 1.5rem;
+      }
+
+      &:last-child {
+        padding-right: 1.5rem;
+      }
+    }
+  }
+`
