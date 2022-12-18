8dd3367825151d2cd2253ce020f6363674d6b45c
José Eduardo Rodrigues Pinto Jerp86 <jerp.dev@gmail.com>
2022-12-17 21:00:27 -0300

    remove comments, add validation with zod, add FormProvider

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,5 +1,8 @@
+import { zodResolver } from '@hookform/resolvers/zod'
 import { HandPalm, Play } from 'phosphor-react'
 import { createContext, useState } from 'react'
+import { FormProvider, useForm } from 'react-hook-form'
+import * as z from 'zod'
 import { NewCycleForm, Countdown } from './components'
 
 import {
@@ -17,6 +20,16 @@ interface Cycle {
   finishedDate?: Date
 }
 
+const newCycleFormValidationSchema = z.object({
+  task: z.string().min(3, 'Informe a tarefa'),
+  minutesAmount: z
+    .number()
+    .min(5, 'O ciclo precisa ser no mínimo 5 minutos. ')
+    .max(60, 'O ciclo precisa ser no máximo 60 minutos.'),
+})
+
+export type NewCycleFormData = z.infer<typeof newCycleFormValidationSchema>
+
 interface CycleContextType {
   activeCycle: Cycle | undefined
   activeCycleId: string | null
@@ -29,6 +42,15 @@ export const Home = () => {
   const [cycles, setCycles] = useState<Cycle[]>([])
   const [activeCycleId, setActiveCycleId] = useState<string | null>(null)
 
+  const newCycleForm = useForm<NewCycleFormData>({
+    resolver: zodResolver(newCycleFormValidationSchema),
+    defaultValues: {
+      task: '',
+      minutesAmount: 0,
+    },
+  })
+
+  const { handleSubmit, reset, watch } = newCycleForm
   const activeCycle = cycles.find(({ id }) => id === activeCycleId)
 
   const markCurrentCycleAsFinished = () => {
@@ -41,24 +63,24 @@ export const Home = () => {
     )
   }
 
-  // const handleCreateNewCycle = ({ minutesAmount, task }: NewCycleFormData) => {
-  //   const id = String(new Date().getTime()) // alterar para crypto.randomUUID()
-  //   const newCycle: Cycle = {
-  //     id,
-  //     task,
-  //     minutesAmount,
-  //     startDate: new Date(),
-  //   }
+  const handleCreateNewCycle = ({ minutesAmount, task }: NewCycleFormData) => {
+    const id = String(new Date().getTime()) // alterar para crypto.randomUUID()
+    const newCycle: Cycle = {
+      id,
+      task,
+      minutesAmount,
+      startDate: new Date(),
+    }
 
-  //   setCycles((state) => [...state, newCycle])
-  //   setActiveCycleId(id)
-  //   setAmountSecondsPassed(0)
+    setCycles((state) => [...state, newCycle])
+    setActiveCycleId(id)
+    // setAmountSecondsPassed(0)
 
-  //   reset()
-  // }
+    reset()
+  }
 
-  // const task = watch('task')
-  // const isSubmitDisabled = !task
+  const task = watch('task')
+  const isSubmitDisabled = !task
 
   const handleInterruptCycle = () => {
     setCycles((state) =>
@@ -74,11 +96,13 @@ export const Home = () => {
 
   return (
     <HomeContainer>
-      <form /* onSubmit={handleSubmit(handleCreateNewCycle)} */>
+      <form onSubmit={handleSubmit(handleCreateNewCycle)}>
         <CycleContext.Provider
           value={{ activeCycle, activeCycleId, markCurrentCycleAsFinished }}
         >
-          <NewCycleForm />
+          <FormProvider {...newCycleForm}>
+            <NewCycleForm />
+          </FormProvider>
           <Countdown />
         </CycleContext.Provider>
 
@@ -88,7 +112,7 @@ export const Home = () => {
             Interromper
           </StopCountdownButton>
         ) : (
-          <StartCountdownButton /* disabled={isSubmitDisabled} */ type="submit">
+          <StartCountdownButton disabled={isSubmitDisabled} type="submit">
             <Play />
             Começar
           </StartCountdownButton>
