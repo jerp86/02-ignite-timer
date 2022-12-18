5f0e2c1db9ad4ae8c99a93d7374fa6f478cf6231
José Eduardo Rodrigues Pinto Jerp86 <jerp.dev@gmail.com>
2022-12-17 21:01:10 -0300

    add useContext and useFormContext

--- src/pages/Home/components/NewCycleForm/index.tsx
+++ src/pages/Home/components/NewCycleForm/index.tsx
@@ -1,35 +1,43 @@
+import { useContext } from 'react'
+import { useFormContext } from 'react-hook-form'
+import { CycleContext } from '../..'
 import { FormContainer, TaskInput, MinutesAmountInput } from './styles'
 
-export const NewCycleForm = () => (
-  <FormContainer>
-    <label htmlFor="task">Vou trabalhar em</label>
-    <TaskInput
-      id="task"
-      list="task-suggestions"
-      placeholder="Dê um nome para o seu projeto"
-      disabled={!!activeCycle}
-      {...register('task')}
-    />
+export const NewCycleForm = () => {
+  const { activeCycle } = useContext(CycleContext)
+  const { register } = useFormContext()
 
-    <datalist id="task-suggestions">
-      <option value="Projeto 1" />
-      <option value="Projeto 2" />
-      <option value="Projeto 3" />
-      <option value="Banana" />
-    </datalist>
+  return (
+    <FormContainer>
+      <label htmlFor="task">Vou trabalhar em</label>
+      <TaskInput
+        id="task"
+        list="task-suggestions"
+        placeholder="Dê um nome para o seu projeto"
+        disabled={!!activeCycle}
+        {...register('task')}
+      />
 
-    <label htmlFor="minutesAmount">durante</label>
-    <MinutesAmountInput
-      type="number"
-      id="minutesAmount"
-      placeholder="00"
-      step={5}
-      min={5}
-      max={60}
-      disabled={!!activeCycle}
-      {...register('minutesAmount', { valueAsNumber: true })}
-    />
+      <datalist id="task-suggestions">
+        <option value="Projeto 1" />
+        <option value="Projeto 2" />
+        <option value="Projeto 3" />
+        <option value="Banana" />
+      </datalist>
 
-    <span>minutos.</span>
-  </FormContainer>
-)
+      <label htmlFor="minutesAmount">durante</label>
+      <MinutesAmountInput
+        type="number"
+        id="minutesAmount"
+        placeholder="00"
+        step={5}
+        min={5}
+        max={60}
+        disabled={!!activeCycle}
+        {...register('minutesAmount', { valueAsNumber: true })}
+      />
+
+      <span>minutos.</span>
+    </FormContainer>
+  )
+}
