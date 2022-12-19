2f9387080e7de8baabd712ab08d6061c50ae28cd
José Eduardo Rodrigues Pinto Jerp86 <jerp.dev@gmail.com>
2022-12-18 23:13:23 -0300

    create ActionTypes enum

--- src/reducers/cycles.ts
+++ src/reducers/cycles.ts
@@ -12,6 +12,12 @@ interface CycleState {
   activeCycleId: string | null
 }
 
+export enum ActionTypes {
+  ADD_NEW_CYCLE = 'ADD_NEW_CYCLE',
+  INTERRUPT_CURRENT_CYCLE = 'INTERRUPT_CURRENT_CYCLE',
+  MARK_CURRENT_CYCLE_AS_FINISHED = 'MARK_CURRENT_CYCLE_AS_FINISHED',
+}
+
 export const cyclesReducers = (state: CycleState, action: any) => {
   switch (action.type) {
     case 'ADD_NEW_CYCLE':
